9db3022515956edfff49058f5b9bbf6ef4a99528
Seong-Hwan Jun <s2jun.uw@gmail.com>
2019-04-08 14:58:13 +0200

    added test directory for likelihood testing. changed the tests accordingly. add codefor generating model selection figures.

--- analysis/model_selection.R
+++ analysis/model_selection.R
@@ -1,27 +1,19 @@
 library(ggplot2)
-x<-c(-1746.62, -1665.55, -1587.11, -1546.76, -1564.41, -1584.4, -1651.98)
-plot(1:length(x), x, type='l')
-points(1:length(x), x, pch=19)
+library(matrixStats)
+library(dplyr)
+setwd("~/Dropbox/Research/single-cell-research/repos/linear-progression/data/model_selection/")
 
-dat<-read.csv("~/Dropbox/Research/single-cell-research/repos/linear-progression/analysis/pathway_samples.csv", header=F)
-colMeans(dat)
-
-params5<-read.csv("~/Dropbox/Research/single-cell-research/repos/linear-progression/analysis/params5.csv", header=F)
-names(params5)<-c("FBP", "BGP")
-p<-ggplot(params5, aes(FBP)) + geom_density()
-p<-p+theme_bw()+ggtitle("Posterior of FBP")
-ggsave("~/Dropbox/Research/single-cell-research/repos/linear-progression/analysis/fbp5.pdf", p)
-
-p<-ggplot(params5, aes(BGP)) + geom_density()
-p<-p+theme_bw()+ggtitle("Posterior of BGP")
-ggsave("~/Dropbox/Research/single-cell-research/repos/linear-progression/analysis/bgp5.pdf", p)
-
-params4<-read.csv("~/Dropbox/Research/single-cell-research/repos/linear-progression/analysis/params4.csv", header=F)
-names(params4)<-c("FBP", "BGP")
-p<-ggplot(params4, aes(FBP)) + geom_density()
-p<-p+theme_bw()+ggtitle("Posterior of FBP")
-ggsave("~/Dropbox/Research/single-cell-research/repos/linear-progression/analysis/fbp4.pdf", p)
-
-p<-ggplot(params4, aes(BGP)) + geom_density()
-p<-p+theme_bw()+ggtitle("Posterior of BGP")
-ggsave("~/Dropbox/Research/single-cell-research/repos/linear-progression/analysis/bgp4.pdf", p)
+true_model_len <- 3
+counts <- rep(0, 10)
+for (rep in 0:99)
+{
+  file_name <- paste("model", true_model_len, "/rep", rep, "/model_selection/fhat.csv", sep="")
+  d <- read.table(file_name, header=F, skip = 1, sep=",")
+  idx <- d[which.max(d$V2),"V1"]
+  counts[idx] = counts[idx] + 1
+  if (idx != true_model_len) {
+    print(rep)
+  }
+}
+df<-data.frame(Model=1:10, Counts=counts)
+ggplot(df, aes(x=Model, y = Counts)) + geom_bar()
