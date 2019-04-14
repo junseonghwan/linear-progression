e21614605659e9c73996c30f11fcb60793395096
Seong-Hwan Jun <s2jun.uw@gmail.com>
2019-04-14 22:07:14 +0200

    removed Experiment data. modified run_pg.py

--- run_pg.py
+++ run_pg.py
@@ -16,19 +16,21 @@ np.random.seed(111)
 curr_dir = os.getcwd()
 
 seed = 1
-n_genes = 25
-n_pg_iter = 20
-n_particles = 200
-n_smc_iter = 2*n_genes
+n_genes = 100
+n_pg_iter = 50
+n_particles = 100
+n_smc_iter = n_genes
 n_kernel_iter = 1
-n_mh_w_gibbs_iter = 10
-has_passenger = False
-swap_prob = 0.2
+n_mh_w_gibbs_iter = 5
+has_passenger = True
+swap_prob = 0.1
 fbp_max = 0
 bgp_max = 0.2
 n_threads = 8
 true_model_len = 5
-rep = 26
+n_patients = 1000
+mh_proposal_sd = 0.1
+rep = 0
 
 _seed = ctypes.c_long(seed)
 _n_genes = ctypes.c_uint(n_genes)
@@ -42,11 +44,12 @@ _swap_prob = ctypes.c_double(swap_prob)
 _fbp_max = ctypes.c_double(fbp_max)
 _bgp_max = ctypes.c_double(bgp_max)
 _n_threads = ctypes.c_uint(n_threads)
+_mh_proposal_sd = ctypes.c_double(mh_proposal_sd)
 _true_model_len = ctypes.c_uint(true_model_len)
 
-data_path = curr_dir + "/data/raphael/25genes/error0.01/rep" + str(rep) + "/"
+data_path = curr_dir + "/data/Experiment3/With_passengers/" + str(n_patients) + "/error0.1/rep" + str(rep) + "/"
 input_path = data_path + "matrix.csv"
-output_path = data_path + "pg"
+output_path = data_path + "pg/" + str(n_patients) + "/"
 true_pathway_file = data_path + "generative_mem_mat.csv"
 true_param_file = data_path + "parameters.csv"
 
@@ -58,7 +61,7 @@ if not os.path.exists(output_path):
 
 lpm_lib.run_pg(_seed, _input_path, _output_path, _true_model_len, _n_pg_iter, 
                 _n_particles, _n_smc_iter, _n_kernel_iter, _n_mh_w_gibbs_iter,
-                _has_passenger, _swap_prob, _fbp_max, _bgp_max)
+                _has_passenger, _swap_prob, _fbp_max, _bgp_max, _mh_proposal_sd)
 
 n_smc_iter = ctypes.c_uint(100)
 fbp = ctypes.c_double(0.01)
